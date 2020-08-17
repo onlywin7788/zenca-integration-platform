86ae2ae151b5de643ec408418a31c1cda10f6b32
navana <onlywin7788@gmail.com>
2020-08-18 01:17:57 +0900

    zull api gateway
    commit

---
+++ zenca-api-gateway/src/test/java/com/navana/zencaapigateway/ZencaApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navana.zencaapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZencaApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
