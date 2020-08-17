2aafbe5935b754da2ddb4dcb31add2c8377f611b
navana <onlywin7788@gmail.com>
2020-08-18 02:58:06 +0900

    zenca-service commit
    commit

---
+++ zenca-service/src/main/java/com/navana/zencaservice/ZencaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.navana.zencaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZencaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZencaServiceApplication.class, args);
+	}
+
+}
