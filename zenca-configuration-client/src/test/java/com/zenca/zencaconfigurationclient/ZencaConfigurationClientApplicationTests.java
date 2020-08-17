e5f5323ca2092f21da6a54ce9f38f9d3ed8a4e16
navana <onlywin7788@gmail.com>
2020-08-17 17:01:32 +0900

    commit config client commit for test

---
+++ zenca-configuration-client/src/test/java/com/zenca/zencaconfigurationclient/ZencaConfigurationClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenca.zencaconfigurationclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZencaConfigurationClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
